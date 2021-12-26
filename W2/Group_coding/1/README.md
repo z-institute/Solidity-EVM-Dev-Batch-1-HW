b8312e7e4c3ab8a434b6a6c01f87517506f93b68
Wang Walter <dreamwalter@Wangde-MacBook-Pro.local>
2021-12-26 22:51:59 +0800

    Home Work 2

--- W2/Group_coding/1/README.md
+++ W2/Group_coding/1/README.md
@@ -1,13 +1,11 @@
 # 客製化參數設計想法
   - Genesis Block 的參數設計影響到整個區塊鏈的設計
 ```
-  difficulty=0x020000,  // POW 難度參數不要設定太高，出塊速度能快些，也減少資源浪費
-  gas_limit=0xffff,     // Gas 上限為65535 Gwei，挖礦難度降低，出塊速度快，挖礦機率增加，Gas下降，讓鏈上交易量更大，更健康
-  gas_used=0x00,
-  extra_data=0x0000000000000000000000000000000000000000000000000000000000000000,
-  mix_hash=0x0000000000000000000000000000000000000000000000000000000000000000,      // ZERO_HASH32
-  nonce=0x00,           // nonce
-  block_number=0x00,    // 初始區塊編號 0
-  parent_hash=0x0000000000000000000000000000000000000000000000000000000000000000,   // ZERO_HASH32,
-  timestamp=0x00,       // 初始區塊出塊時間
+GENESIS_PARAMS = {
+  'difficulty': 0x02,     // POW 難度參數不要設定太高，出塊速度能快些，也減少資源浪費
+  'gas_limit' : 0xffff,   // Gas 上限為65535 Gwei，挖礦難度降低，出塊速度快，挖礦機率增加，Gas下降，讓鏈上交易量更大，更健康
+  'nonce' : bytes(0),
+  'timestamp' : 0x00,     // 初始區塊出塊時間
+  'extra_data': bytes('This blockchain is created on 2021/12/25 00:00 by Ethan, Walter, Lemonde.', 'utf-8'),
+}
 ```
--- W2/Group_coding/1/demo-app/app/main.py
+++ W2/Group_coding/1/demo-app/app/main.py
@@ -1,29 +1,43 @@
-from eth_keys import keys
-from eth import constants
 from eth.chains.mainnet import MainnetChain
 from eth.db.atomic import AtomicDB
-from eth_utils import to_wei, encode_hex
+from eth_typing import Address
+from eth_utils import to_wei
 
-# Giving funds to some address
-SOME_ADDRESS = b'\x85\x82\xa2\x89V\xb9%\x93M\x03\xdd\xb4Xu\xe1\x8e\x85\x93\x12\xc1'
+ETHAN_ADDRESS = Address(20 * b'\x11')
+WALTER_ADDRESS = Address(20 * b'\x22')
+LEMONDE_ADDRESS = Address(20 * b'\x33')
+
+FOUNDER_INITIAL_BALANCE = to_wei(1000000, 'ether')
+GENESIS_PARAMS = {
+     # accelerate mining
+    'difficulty': 0x02,
+    'gas_limit' : 0xffff,
+    'nonce' : bytes(0),
+    'timestamp' : 0x00,
+    # add data like bitcoin genesis block
+    'extra_data': bytes('This blockchain is created on 2021/12/25 00:00 by Ethan, Walter, Lemonde.', 'utf-8'),
+}
+
+# send founders 1000000 ETH
 GENESIS_STATE = {
-    SOME_ADDRESS: {
-        "balance": to_wei(99, 'ether'),
+    ETHAN_ADDRESS: {
+        "balance": FOUNDER_INITIAL_BALANCE,
         "nonce": 0,
         "code": b'',
         "storage": {}
-    }
-}
-
-GENESIS_PARAMS = {
-    'difficulty': constants.GENESIS_DIFFICULTY,
+    },
+    WALTER_ADDRESS: {
+        "balance": FOUNDER_INITIAL_BALANCE,
+        "nonce": 0,
+        "code": b'',
+        "storage": {}
+    },
+    LEMONDE_ADDRESS: {
+        "balance": FOUNDER_INITIAL_BALANCE,
+        "nonce": 0,
+        "code": b'',
+        "storage": {}
+    },
 }
 
 chain = MainnetChain.from_genesis(AtomicDB(), GENESIS_PARAMS, GENESIS_STATE)
-
-current_vm = chain.get_vm()
-state = current_vm.state
-
-print("The balance is {} wei".format(
-    state.get_balance(SOME_ADDRESS)
-))
