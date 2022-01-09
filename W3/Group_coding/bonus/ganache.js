f81ec5586c4e64cf51e920f2c873c98d4866baf8
abner.huang <abner.huang@titansoft.com.sg>
2022-01-09 23:37:45 +0800

    update ganache.js

--- W3/Group_coding/bonus/ganache.js
+++ W3/Group_coding/bonus/ganache.js
@@ -1,10 +1,16 @@
 const ganache = require('ganache');
-
 const options = {};
-const server = ganache.server(options);
+const server = ganache.server({
+  fork: {
+    url: 'https://mainnet.infura.io/v3/128da609d4284200abfb61efc6ebc545',
+  },
+  chain: {
+    chainId: 1
+  }
+});
 const provider = server.provider;
 const PORT = 8545;
-server.listen(PORT, (err) => {
+server.listen(PORT, '0.0.0.0', (err) => {
   if (err) throw err;
 
   console.log(`ganache listening on port ${PORT}...`);
