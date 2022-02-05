2410c76add5faac6ec764447bc924bb02c9ec1a9
Wang Walter <dreamwalter@Wangde-MacBook-Pro.local>
2022-02-05 18:21:08 +0800

    Home Work 6-2

---
+++ W6-2/individual/ERC721A/hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-gas-reporter');
+
+if (process.env.REPORT_GAS) {
+  require("hardhat-gas-reporter");
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 100,
+    showTimeSpent: true,
+  },
+};
